b3d35940cfa47b66d2968cb9f7b301a9e98f3aa1
Shriyanshu27 <114811340+Shriyanshu27@users.noreply.github.com>
2024-03-06 15:09:12 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,34 @@
+
+
+import express from "express";
+import mongoose from "mongoose";
+import cors from "cors";
+import morgan from "morgan";
+import userRouter from "./routes/user.js";
+import tourRouter from "./routes/tour.js";
+
+const app = express();
+
+app.use(morgan("dev"));
+app.use(express.json({ limit: "30mb", extended: true }));
+app.use(express.urlencoded({ limit: "30mb", extended: true }));
+app.use(cors());
+
+app.use("/users", userRouter); // http://localhost:5000/user/signup
+
+app.use("/tour", tourRouter);
+
+const MONGODB_URL = 
+"link_plz";
+
+const port = 8000;
+
+mongoose
+  .connect(MONGODB_URL)
+  .then(() => {
+    app.listen(port, () => console.log(`Server running on port ${port}`));
+  })
+  .catch((error) => console.log(`${error} did not connect`));
+
+
+  
